20b9963365a514200704811a1357aeec9254f7cf
himesht <60806145+himesht@users.noreply.github.com>
2020-07-21 23:34:36 +0530

    first commit

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
 
   const firstName = req.body.fname;
   const lastName = req.body.lname;
@@ -37,13 +37,13 @@ app.post("/", function(req, res) {
   const url = "https://us8.api.mailchimp.com/3.0/lists/a10f05c354";
   const options = {
     method: "POST",
-    auth: "HT30:4de6f52ac39232a9ab01234cacbc47e1-us8"
+    auth: "username:api_key"
   }
-  const request = https.request(url, options, function(response) {
+  const request = https.request(url, options, function (response) {
     console.log(response.statusCode);
 
 
-    response.on("data", function(data) {
+    response.on("data", function (data) {
       const dataDetails = JSON.parse(data);
       console.log(dataDetails);
 
@@ -58,13 +58,13 @@ app.post("/", function(req, res) {
   request.end();
 });
 
-app.post("/failure", function(req, res) {
+app.post("/failure", function (req, res) {
   res.redirect("/");
 });
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
   console.log("server is running on 3000");
 });
 
-//80e3bc2279b5b38143de85df97598f28-us8.  For mail chimp
 
-//Audience list id a10f05c354
+
+
